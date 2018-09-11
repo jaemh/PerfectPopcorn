bd5b17955ab3317a61787d38d7f1f348f489930e
jaem <jaana.embrich-hakala@helsinki.fi>
2018-09-11 14:23:14 +0300

    tasks

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,23 @@
+#Tuodaan Flask käyttöön
 from flask import Flask
 app = Flask(__name__)
 
+
+#Tuodaan SQLAlchemy käyttöön
+from flask_sqlalchemy import SQLAlchemy
+
+#Käytetään tasks.db-nimistä SQLite -tietokantaa.
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks.db"
+#Pyydetään SQLAlchemyä tuomaan kaikki SQL -kyselyt
+app.config["SQLALCHEMY_ECHO"] = True
+#Luodaan db-olio jota käytetään tietokannan käsittelyyn
+db = SQLAlchemy(app)
+
+#Luetaan kansiosta application tietoston views sisältö
 from application import views
+
+#Tuodaan models luokka application käyttöön
+from application.tasks import models
+
+#Luodaan lopulta tarvittavat tietokantataulut
+db.create_all()
