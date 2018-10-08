4a30c29ea29d3e7c577baa31f6a4d993b73d235c
jaem <jaana.embrich-hakala@helsinki.fi>
2018-10-08 11:05:44 +0300

    korjattu kirjautuminen

--- application/movies/models.py
+++ application/movies/models.py
@@ -2,8 +2,8 @@ from application import db
 
 class Movie(db.Model):
     __tablename__ = 'movie'
-    movie_id = db.Column(db.Integer, primary_key=True)
-    movie_name = db.Column(db.String(20), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
 
 
 def __repr__(self):
