4cfa7ec2c229a1e974610e840155bcaad8ac05f0
jaem <jaana.embrich-hakala@helsinki.fi>
2018-10-08 13:14:33 +0300

    lisätty elokuvia

--- application/movies/views.py
+++ application/movies/views.py
@@ -7,3 +7,23 @@ from application import app
 def all_action_movies():
     movies = Movie.query.all()
     return render_template("movies/action.html", movies=movies)
+
+@app.route("/comedy", methods=["GET"])
+def all_comedy_movies():
+    movies = Movie.query.all()
+    return render_template("movies/comedy.html", movies=movies)
+
+@app.route("/fantacy", methods=["GET"])
+def all_fantacy_movies():
+    movies = Movie.query.all()
+    return render_template("movies/fantacy.html", movies=movies)
+
+@app.route("/drama", methods=["GET"])
+def all_drama_movies():
+    movies = Movie.query.all()
+    return render_template("movies/drama.html", movies=movies)
+
+@app.route("/historical", methods=["GET"])
+def all_historical_movies():
+    movies = Movie.query.all()
+    return render_template("movies/historical.html", movies=movies)
