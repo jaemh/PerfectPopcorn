b1d07eed91a0f4c6195f9b9891996700e9527ad9
jaem <jaana.embrich-hakala@helsinki.fi>
2018-10-13 08:57:27 +0300

    movies

--- application/movies/models.py
+++ application/movies/models.py
@@ -4,8 +4,18 @@ class Movie(db.Model):
     __tablename__ = 'movie'
     movie_id = db.Column(db.Integer, primary_key=True)
     movie_name = db.Column(db.String(20), nullable=False)
-    
-    def __init__(self, name):
+    genre_id = db.Column(db.Integer, db.ForeignKey('genre.genre_id'), nullable=False)
+    genre = db.relationship('Genre', backref='genre')   
+
+    def __init__(self, name, genre):
         self.movie_name=name
+        self.genre = genre
     
-    
+class Genre(db.Model):
+    __tablename__ = 'genre'
+    genre_id = db.Column(db.Integer, primary_key=True)
+    genre_name = db.Column(db.String)
+
+    def __init__(self, name):
+        self.genre_name=name
+
--- application/movies/views.py
+++ application/movies/views.py
@@ -3,20 +3,18 @@ from application.auth.forms import RegistrationForm
 from application import app, db
 from application.auth.models import User
 from application.movies.forms import MovieForm
-from application.movies.models import Movie
+from application.movies.models import Movie, Genre
 from flask_login import current_user, login_required
 
 
 @app.route("/person", methods=["GET"])
 def person_page():
     user = User.query.get(current_user.id)
-
     return render_template("movies/page.html", user=user)
 
 @app.route("/edit")
 def person_edit():
     form = RegistrationForm(request.form)
-
     return render_template("auth/signup.html", form = RegistrationForm())
 
 @app.route("/person/new/")
@@ -29,8 +27,8 @@ def create_movie():
     f = Movie.query.filter_by(movie_name=request.form.get('name')).first()
 
     if f is None: 
-        f = Movie(request.form.get('name'))
-    
+        f = Movie(request.form.get('name'), Genre.query.get(request.form.get('genre')))
+
     userId = current_user.id
     user = User.query.get(userId)
     user.seenMovies.append(f) #tekee liittämisen
